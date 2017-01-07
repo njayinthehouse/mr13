ac9c1db98f6646163e0587e45edf8de525ac981f
njay <nitinjohnraj@gmail.com>
2017-01-07 18:04:41 +0530

    Undid the hard-coded genres, places genres in a separate file instead.

--- src/sqlalchemy_declarative.py
+++ src/sqlalchemy_declarative.py
@@ -14,15 +14,10 @@ manga = Table('manga', metadata,
               Column('summary', Text)
               )
 
-genres = Table('genres', metadata,
-               Column('id', Integer, primary_key=True),
-               Column('value', String(256), unique=True)
-               )
-
 genre_mappings = Table('genremappings', metadata,
                        Column('id', Integer, primary_key=True),
                        Column('manga_id', None, ForeignKey('manga.id')),
-                       Column('genre_id', None, ForeignKey('genres.id'))
+                       Column('genre_id', None, Integer)
                        )
 
 chapters = Table('chapters', metadata,
@@ -40,45 +35,3 @@ tracking = Table('tracking', metadata,
                  )
 
 metadata.create_all(engine)
-
-# Hard coded values
-connection = engine.connect()
-connection.execute(genres.insert(), [
-    {'id': 1, 'value': 'Action'},
-    {'id': 2, 'value': 'Adventure'},
-    {'id': 3, 'value': 'Comedy'},
-    {'id': 4, 'value': 'Demons'},
-    {'id': 5, 'value': 'Drama'},
-    {'id': 6, 'value': 'Ecchi'},
-    {'id': 7, 'value': 'Fantasy'},
-    {'id': 8, 'value': 'Gender Bender'},
-    {'id': 9, 'value': 'Harem'},
-    {'id': 10, 'value': 'Historical'},
-    {'id': 11, 'value': 'Horror'},
-    {'id': 12, 'value': 'Josei'},
-    {'id': 13, 'value': 'Magic'},
-    {'id': 14, 'value': 'Martial Arts'},
-    {'id': 15, 'value': 'Mature'},
-    {'id': 16, 'value': 'Mecha'},
-    {'id': 17, 'value': 'Military'},
-    {'id': 18, 'value': 'Mystery'},
-    {'id': 19, 'value': 'One Shot'},
-    {'id': 20, 'value': 'Psychological'},
-    {'id': 21, 'value': 'Romance'},
-    {'id': 22, 'value': 'School Life'},
-    {'id': 23, 'value': 'Sci-Fi'},
-    {'id': 24, 'value': 'Seinen'},
-    {'id': 25, 'value': 'Shoujo'},
-    {'id': 26, 'value': 'Shoujoai'},
-    {'id': 27, 'value': 'Shounen'},
-    {'id': 28, 'value': 'Shounenai'},
-    {'id': 29, 'value': 'Slice of Life'},
-    {'id': 30, 'value': 'Smut'},
-    {'id': 31, 'value': 'Sports'},
-    {'id': 32, 'value': 'Super Power'},
-    {'id': 33, 'value': 'Supernatural'},
-    {'id': 34, 'value': 'Tragedy'},
-    {'id': 35, 'value': 'Vampire'},
-    {'id': 36, 'value': 'Yaoi'},
-    {'id': 37, 'value': 'Yuri'},
-])
